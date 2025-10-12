6354047ccafd2f51486f635efecfaeebbcae9f6a
mabdullahjs <mabdullah2037@outlook.com>
2025-10-12 12:49:48 +0500

    functions in js

---
+++ 02-Javascript/05-Functions/app.js
@@ -0,0 +1,39 @@
+// console.log("hello world");
+
+
+
+
+
+// function testing() {
+//     console.log("hello world");
+//     console.log("hello world");
+//     console.log("hello world");
+//     console.log("hello world");
+// }
+
+
+// testing()
+
+
+
+
+
+// function greetUser(){
+//     console.log("hello abdullah");
+    
+// }
+
+// greetUser()
+
+
+
+// argument
+// parameter
+
+function greetUser(username){ // parameter receive krna
+    console.log("hello " + username);
+    
+}
+
+greetUser("abdullah")
+greetUser("Usman") // argument pass krna
