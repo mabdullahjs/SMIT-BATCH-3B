215995261e2375f9c077daad60bbd019f8ecfaa9
mabdullahjs <mabdullah2037@outlook.com>
2025-10-18 13:03:23 +0500

    function return

--- 02-Javascript/05-Functions/app.js
+++ 02-Javascript/05-Functions/app.js
@@ -1,9 +1,5 @@
 // console.log("hello world");
 
-
-
-
-
 // function testing() {
 //     console.log("hello world");
 //     console.log("hello world");
@@ -11,29 +7,112 @@
 //     console.log("hello world");
 // }
 
-
 // testing()
-
-
-
-
 
 // function greetUser(){
 //     console.log("hello abdullah");
-    
+
 // }
 
 // greetUser()
 
+// argument
+// parameter
 
+// function greetUser(username){ // parameter receive krna
+//     console.log("hello " + username);
+
+// }
+
+// greetUser("abdullah")
+// greetUser("Usman") // argument pass krna
+
+// console.log("Hello world 1");
+// console.log("Hello world 2");
+// console.log("Hello world 3");
+// console.log("Hello world 4");
+// console.log("Hello world 5");
+
+// alert()
+// prompt()
+
+// function reuseable() {
+//   console.log("Hello world 1");
+//   console.log("Hello world 2");
+//   console.log("Hello world 3");
+//   console.log("Hello world 4");
+//   console.log("Hello world 5");
+// }
+
+// reuseable()
+// reuseable()
+// reuseable()
 
 // argument
 // parameter
 
-function greetUser(username){ // parameter receive krna
-    console.log("hello " + username);
-    
+// function greetUser(username){
+//     console.log("hello " + username);
+
+// }
+
+// greetUser("abdullah") // argument pass krna
+
+// greetUser("Usman")
+
+// function greetUser(username , email , age) {
+//     console.log("hello " + username + " email " + email + " age " + age);
+
+// }
+
+// greetUser("abdullah" , "abdullah@gmail.com" , 21)
+
+// function sum(num1 , num2){
+//     console.log("total is ==>" , num1 + num2);
+
+// }
+
+// sum(20 - 5 , 20)
+// sum(30 , 10)
+
+// undefined
+// null
+
+// let username = "ada";
+// console.log(username);
+
+// let username = null
+// console.log(username);
+
+// testing()
+
+// return
+
+// let username = prompt("enter your name");
+
+// console.log(prompt("enter your age"));
+
+// console.log(username);
+
+// return ka matlb jaha function call hua ha waha return hui wi value ajayegi
+
+// return ka baad koi code nahi likhta return func ka last ma ata ha
+// return fucntion ma aik dafa hota ha
+
+// function testing(){
+//     console.log("data");
+//     return "return kr rha hon"
+// }
+
+// let username = testing();
+
+// console.log(testing());
+
+// console.log(username);
+
+function greetUser(username) {
+  console.log("hello " + username);
 }
 
 greetUser("abdullah")
-greetUser("Usman") // argument pass krna
+// console.log(greetUser("abdullah"));
