e96420f2866a847fabf25b79141c24ef15aeee2e
mabdullahjs <mabdullah2037@outlook.com>
2025-11-08 12:49:56 +0500

    todo app

---
+++ 02-Javascript/08-Todo-App/app.js
@@ -0,0 +1,41 @@
+// CRUD
+
+// create
+// read
+// update
+// delete
+
+const input = document.querySelector("#input");
+const ol = document.querySelector("#ol");
+
+const allTodos = [];
+
+function renderTodo() {
+  ol.innerHTML = "";
+  for (let i = 0; i < allTodos.length; i++) {
+    ol.innerHTML += `<li>${allTodos[i]}
+        <button onclick="deleteTodo(${i})">delete</button>
+        <button onclick="editTodo(${i})">edit</button>
+        </li>`;
+  }
+}
+function addTodo() {
+  allTodos.push(input.value);
+
+  renderTodo();
+  input.value = "";
+}
+
+function deleteTodo(index) {
+  console.log("todo deleted", index);
+  allTodos.splice(index, 1);
+  renderTodo();
+}
+function editTodo(index) {
+  console.log("todo edited", index);
+  const updatedVal = prompt("enter updated value", allTodos[index]);
+  //   allTodos.splice(index , 1 , updatedVal)
+  allTodos[index] = updatedVal
+
+  renderTodo()
+}
